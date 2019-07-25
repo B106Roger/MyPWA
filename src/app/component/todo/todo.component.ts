ebe59b4a06afaf214d6be582d4ad847c8efabc59
106roger <imbigking12@gmail.com>
2019-07-25 16:50:56 +0800

    commit 一下

--- src/app/component/todo/todo.component.ts
+++ src/app/component/todo/todo.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
+  todoList = [
+    {
+      message: 'buy a unicorn',
+      complete: false,
+    },
+    {
+      message: 'buy a elephant',
+      complete: true,
+    }
+  ];
+  inputValue = '';
+  showType = 0;
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  destroyItem(index: number) {
+    this.todoList.splice(index, 1);
+  }
+  toggleItem(index: number) {
+    this.todoList[index].complete = !this.todoList[index].complete;
+  }
+  toggleAll(value: boolean) {
+    for(let i = 0 ; i < this.todoList.length; i++ ) {
+      this.todoList[i].complete = value;
+    }
+  }
+  addItem(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.todoList.push({
+        message: this.inputValue,
+        complete: false
+      });
+      this.inputValue = '';
+    }
+  }
 }
