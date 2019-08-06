3efba6ccdcb50adda0eb774952d2030ca919342a
106roger <imbigking12@gmail.com>
2019-08-06 23:08:20 +0800

    更新test

--- src/app/component/load/load.component.ts
+++ src/app/component/load/load.component.ts
@@ -16,11 +16,28 @@ var PARENT_USER       = "test005";
 export class LoadComponent implements OnInit, AfterViewInit {
   msg1 = '';
   msg2 = '';
+  msg3 = '';
+  msg4 = '';
+  msg5 = '';
+  msg6 = '';
   ngOnInit() {
+    this.update();
   }
   ngAfterViewInit() {
+    this.update();
   }
-
+  update() {
+    this.msg1 = `window.innerHeight: ${window.innerHeight} px`;
+    this.msg2 = `window.innerWidth: ${window.innerWidth} px`;
+    this.msg3 = `window.outerHeight: ${window.outerHeight} px`;
+    this.msg4 = `window.outerWidth: ${window.outerWidth} px`;
+    this.msg5 = `screen.height: ${screen.height} px`;
+    this.msg6 = `screen.width: ${screen.width} px`;
+  }
+  updateWithAlert() {
+    this.update();
+    window.alert('update');
+  }
   innerHeight() {
     this.msg2 = `window.innerHeight: ${window.innerHeight} px`;
   }
@@ -32,6 +49,12 @@ export class LoadComponent implements OnInit, AfterViewInit {
   }
   outerWidth() {
     this.msg1 = `window.outerWidth: ${window.outerWidth} px`;
+  }
+  screenHeight() {
+    this.msg2 = `screen.height: ${screen.height} px`;
+  }
+  screenWidth() {
+    this.msg1 = `screen.width: ${screen.width} px`;
   }
 
 }
